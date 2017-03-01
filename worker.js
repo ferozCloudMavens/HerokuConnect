ac06160e8f0932406698c1300f9f7f4062e6080f
ferozmuslim <ferozmuslim@gmail.com>
2017-03-01 10:16:31 +0000

    .toLowerCase()

--- worker.js
+++ worker.js
@@ -21,7 +21,7 @@ pg.connect(process.env.DATABASE_URL+'?ssl=true', function(err, client, done) {
     } else {
       var contacts = {};
       result.rows.forEach(function(row){
-        contacts[row.twitter_handle__c] = row;
+        contacts[row.twitter_handle__c.toLowerCase()] = row;
       });
       console.log('contacts :', contacts);
 
@@ -42,15 +42,13 @@ pg.connect(process.env.DATABASE_URL+'?ssl=true', function(err, client, done) {
           tw.stream('statuses/filter', {track: query}, function(stream) {
             stream.on('data', function(tweet) {
               console.log('Tweet: ',tweet);
-              console.log('contacts[tweet.user.screen_name] is: ' + contacts[tweet.user.screen_name]);
-              console.log('contacts[tweet.user.screen_name.toLowerCase()] is: ' + contacts[tweet.user.screen_name.toLowerCase()]);
-              if (contacts[tweet.user.screen_name]) {
+              if (contacts[tweet.user.screen_name.toLowerCase()]) {
                 campaigns.forEach(function(campaign){
                   if (tweet.text.toLowerCase().indexOf(campaign.hashtag__c.toLowerCase()) !== -1) {
-                    console.log('Inserting: ', tweet.id_str, contacts[tweet.user.screen_name].sfid, campaign.sfid, tweet.text);
+                    console.log('Inserting: ', tweet.id_str, contacts[tweet.user.screen_name.toLowerCase()].sfid, campaign.sfid, tweet.text);
                     var insert = 'INSERT INTO salesforce.tweet__c(name, contact__c, campaign__c, text__c) '+
                                  'VALUES($1, $2, $3, $4)';
-                    client.query(insert, [tweet.id_str, contacts[tweet.user.screen_name].sfid, campaign.sfid, tweet.text], function(err, result) {
+                    client.query(insert, [tweet.id_str, contacts[tweet.user.screen_name.toLowerCase()].sfid, campaign.sfid, tweet.text], function(err, result) {
                       if(err) {
                         console.error(err);
                       } else {
